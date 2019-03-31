80899a08f09a79026186fa575791c58ff0446dca
clevmania <ernestcleverest@gmail.com>
2019-03-31 22:00:28 +0100

    wiring the activity to the adapter

--- app/src/main/java/com/clevmania/onboarding/IntroActivity.kt
+++ app/src/main/java/com/clevmania/onboarding/IntroActivity.kt
@@ -2,11 +2,20 @@ package com.clevmania.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var onBoardPager: ViewPager
+    private lateinit var introAdapter: IntroAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        initViews()
+    }
+
+    private fun initViews(){
+        vp_pager.adapter = IntroAdapter(this)
     }
 }
