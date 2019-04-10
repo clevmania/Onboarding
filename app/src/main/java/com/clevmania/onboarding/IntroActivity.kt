caf52a3b8c67dd060fc12290bd7db56bab45f1e5
clevmania <ernestcleverest@gmail.com>
2019-04-10 03:44:38 +0100

    added nav layout and display onboard in fullscreen

--- app/src/main/java/com/clevmania/onboarding/IntroActivity.kt
+++ app/src/main/java/com/clevmania/onboarding/IntroActivity.kt
@@ -2,6 +2,8 @@ package com.clevmania.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_intro.*
 
@@ -11,11 +13,14 @@ class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_intro)
         initViews()
     }
 
     private fun initViews(){
+        tl_indicator.setupWithViewPager(vp_pager)
         vp_pager.adapter = IntroAdapter(this)
     }
 }
